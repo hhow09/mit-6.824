dfbe1d2d2d9731c95a17bf995477dda99c4d6fd5
hhow09 <hhow09@gmail.com>
2024-07-27 23:01:23 +0800

    feat: pass TestInitialElection2A - implement vote request - implement vote request handler - implement state machine and timer - lablog for debug

---
+++ src/lablog/lablog.go
@@ -0,0 +1,58 @@
+package lablog
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"time"
+)
+
+// Retrieve the verbosity level from an environment variable
+// ref: https://blog.josejg.com/debugging-pretty/
+func getVerbosity() int {
+	v := os.Getenv("VERBOSE")
+	level := 0
+	if v != "" {
+		var err error
+		level, err = strconv.Atoi(v)
+		if err != nil {
+			log.Fatalf("Invalid verbosity %v", v)
+		}
+	}
+	return level
+}
+
+type LogTopic string
+
+const (
+	Heart  LogTopic = "HART"
+	Info   LogTopic = "INFO"
+	Config LogTopic = "CONF"
+	Vote   LogTopic = "VOTE"
+	Timer  LogTopic = "TIMR"
+	Leader LogTopic = "LEAD"
+)
+
+var debugStart time.Time
+var debugVerbosity int
+
+func init() {
+	debugVerbosity = getVerbosity()
+	debugStart = time.Now()
+	// disable datetime logging
+	log.SetFlags(log.Flags() &^ (log.Ldate | log.Ltime))
+}
+
+func Debug(serverId int, topic LogTopic, format string, a ...interface{}) {
+	if debugVerbosity == 1 {
+		time := time.Since(debugStart).Microseconds()
+		time /= 100
+		prefix := fmt.Sprintf("%06d %v ", time, string(topic))
+		if serverId >= 0 {
+			prefix += fmt.Sprintf("S%d ", serverId)
+		}
+		format = prefix + format
+		log.Printf(format, a...)
+	}
+}
---
+++ src/test.sh
@@ -0,0 +1,11 @@
+set -e
+rm -f log.txt
+
+TIMES=10
+for i in $(seq 1 $TIMES); do
+  echo "Running test 2A-$i"
+  VERBOSE=1 go test ./raft/... -race -run "(TestInitialElection2A)" -count=1 >> log.txt
+done
+
+
+
